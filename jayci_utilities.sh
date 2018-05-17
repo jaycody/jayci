3265d924118cfbd0804db399eda66a8dc8953973
Jason Stephens <videoalchemy@gmail.com>
2018-05-17 04:31:07 -0700

    Create jayci_utilities.sh

---
+++ jayci_utilities.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+
+# Check the source pipeline
+jayci_test () {
+  jayci_announce "jayci utility functions sourced: CHECK"
+}
+
+
+
+
+######### CONSOLE LOGGING UTILITIES ###########
+
+# Write something to the console in red.
+jayci_error () {
+    local WHITE_ON_RED="\e[7;31m"
+    local NORMAL="\e[0m"
+    local LINE1=${1:-}
+    local LINE2=${2:-}
+
+    printf "${WHITE_ON_RED}======================================================================${NORMAL}\n"
+    printf "${WHITE_ON_RED}  DSP:  %-60s  ${NORMAL}\n" "${LINE1}"
+    if [ "${LINE2}" != "" ]
+    then
+        printf "${WHITE_ON_RED}  DSP:  %-60s  ${NORMAL}\n" "${LINE2}"
+    fi
+    printf "${WHITE_ON_RED}----------------------------------------------------------------------${NORMAL}\n"
+}
+
+# Write something to the console in blue.
+jayci_announce () {
+    local WHITE_ON_BLUE="\e[7;34m"
+    local NORMAL="\e[0m"
+    local LINE1=${1:-}
+    local LINE2=${2:-}
+
+    printf "${WHITE_ON_BLUE}======================================================================${NORMAL}\n"
+    printf "${WHITE_ON_BLUE}  DSP:  %-60s  ${NORMAL}\n" "${LINE1}"
+    if [ "${LINE2}" != "" ]
+    then
+        printf "${WHITE_ON_BLUE}  DSP:  %-60s  ${NORMAL}\n" "${LINE2}"
+    fi
+    printf "${WHITE_ON_BLUE}----------------------------------------------------------------------${NORMAL}\n"
+}
